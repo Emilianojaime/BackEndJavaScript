fd104c8895a1e3035585d5085241f3e679371463
Emiliano Jaime <emilianojaime123@gmail.com>
2021-04-19 09:44:25 -0300

    final corrections done, some console logs deleted

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const uuid = require('uuid');
 const Port = process.env.Port || 4000;
+const cors = require('cors');
 app.listen(Port, () => console.log(`Server started on port ${Port}`));
 const fs = require('fs');
+app.use(cors());
 
 //Array for members
 
@@ -17,17 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 
 //Register Member
 app.post('/api/register', (req, res) => {
+  
+  console.log('Console log',req.body);
   const newMember = {
     id: uuid.v4(),
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   }
+  console.log(newMember);
   members.push(newMember);
   const json_members = JSON.stringify(members);
   fs.writeFileSync('members.json', json_members, 'utf-8');
-  res.send({ msg: "User registered"});
-
+  res.json({msg: 'User has been registered '})
 });
 
 
@@ -36,7 +40,6 @@ app.put('/api/login', (req, res) => {
 const found = members.some(members => 
  members.email === req.body.email && members.password === req.body.password
 );
-console.log(req.body.username);
 console.log(found);
 if (found) {
   res.status(200).json({ msg: "User found, login successful!" });
